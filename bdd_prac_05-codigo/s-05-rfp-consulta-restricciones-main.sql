7915deae7b438fe4ead90e8ca7a0f909fcf97231
Rodrigo <rhodfra@gmail.com>
2020-11-04 23:00:41 -0600

    Fix typo in connect command

--- bdd_prac_05-codigo/s-05-rfp-consulta-restricciones-main.sql
+++ bdd_prac_05-codigo/s-05-rfp-consulta-restricciones-main.sql
@@ -3,12 +3,12 @@
 --@Descripcion: Consulta de restricciones de referencia en rfp-pc
 
 Prompt Conectando a S1 - rfpbd_s1
-connect bancos_bdd/editorial_bdd@rfpbd_s1
+connect editorial_bdd/editorial_bdd@rfpbd_s1
 --ejecuta la misma consulta en ambas pdbs
 @s-05-rfp-consulta-restricciones.sql
 
 Prompt Conectando a S2 - rfpbd_s2
-connect bancos_bdd/editorial_bdd@rfpbd_s2
+connect editorial_bdd/editorial_bdd@rfpbd_s2
 --ejecuta la misma consulta en ambas pdbs
 @s-05-rfp-consulta-restricciones.sql
 
