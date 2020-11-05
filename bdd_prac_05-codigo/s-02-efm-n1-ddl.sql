3d165b4721d520b4e3a25c6017d8bea96d224265
Emanuel Flores <mufymes@gmail.com>
2020-11-05 11:56:22 -0600

    Added EFM DDL N2 script

--- bdd_prac_05-codigo/s-02-efm-n1-ddl.sql
+++ bdd_prac_05-codigo/s-02-efm-n1-ddl.sql
@@ -1,4 +1,4 @@
---@Author:	    	Emanuel Flores Martínez
+--@Author:	    Emanuel Flores Martínez
 --@Fecha creación:  05/11/2020
 --@Descripción:     Implementación de fragmentos en N1 - Código DDL
 
@@ -16,7 +16,7 @@ begin
 	where table_name = name;
 	
 	if (v_count > 0) then
-       execute immediate 'drop table ' || name || ' cascade constraints';
+       		execute immediate 'drop table ' || name || ' cascade constraints';
 	end if;
 end;
 /
@@ -24,12 +24,12 @@ end;
 declare
 	v_articulo_revista1 varchar2(30):= 'F_EFM_ARTICULO_REVISTA_1';
 	v_pago_suscriptor1 varchar2(30)	:= 'F_EFM_PAGO_SUSCRIPTOR_1';
-	v_suscriptor1 varchar2(30) 		:= 'F_EFM_SUSCRIPTOR_1';
-	v_suscriptor2 varchar2(30)		:= 'F_EFM_SUSCRIPTOR_2';
-	v_suscriptor3 varchar2(30) 		:= 'F_EFM_SUSCRIPTOR_3';
-	v_articulo2 varchar2(30) 		:= 'F_EFM_ARTICULO_2';
-	v_revista1 varchar2(30) 		:= 'F_EFM_REVISTA_1';
-	v_pais1 varchar2(30) 			:= 'F_EFM_PAIS_1';
+	v_suscriptor1 varchar2(30) 	:= 'F_EFM_SUSCRIPTOR_1';
+	v_suscriptor2 varchar2(30)	:= 'F_EFM_SUSCRIPTOR_2';
+	v_suscriptor3 varchar2(30) 	:= 'F_EFM_SUSCRIPTOR_3';
+	v_articulo2 varchar2(30) 	:= 'F_EFM_ARTICULO_2';
+	v_revista1 varchar2(30) 	:= 'F_EFM_REVISTA_1';
+	v_pais1 varchar2(30) 		:= 'F_EFM_PAIS_1';
 begin
 	drop_table_if_exists(v_articulo_revista1);
 	drop_table_if_exists(v_pago_suscriptor1);
@@ -100,7 +100,7 @@ create table f_efm_suscriptor_2 (
     fecha_inscripcion    date             not null,
     pais_id              number(3, 0)     not null,
     constraint f_efm_suscriptor_2_pk primary key (suscriptor_id),
-	constraint f_efm_suscriptor_2_pais_id_fk foreign key (pais_id)
+    constraint f_efm_suscriptor_2_pais_id_fk foreign key (pais_id)
     references f_efm_pais_1 (pais_id)
 );
 
@@ -123,7 +123,7 @@ create table f_efm_pago_suscriptor_1 (
     importe          number(8, 2)     not null,
     recibo_pago      blob             not null,
     constraint f_efm_pago_suscriptor_1 primary key (num_pago, suscriptor_id),
-	constraint f_efm_pago_suscriptor_1_suscriptor_id_fk foreign key (suscriptor_id)
+    constraint pago_suscriptor_1_suscriptor_id_fk foreign key (suscriptor_id)
     references f_efm_suscriptor_1 (suscriptor_id)
 );
 
@@ -137,7 +137,7 @@ create table f_efm_articulo_revista_1 (
     revista_id             number(10, 0)    not null,
     articulo_id            number(10, 0)    not null,
     constraint f_efm_articulo_revista_1_pk primary key (articulo_revista_id),
-	constraint f_efm_articulo_revista_1_revista_id_fk foreign key (revista_id)
+    constraint f_efm_articulo_revista_1_revista_id_fk foreign key (revista_id)
     references f_efm_revista_1 (revista_id),
     constraint f_efm_articulo_revista_1_articulo_id_fk foreign key (articulo_id)
     references f_efm_articulo_2 (articulo_id)
