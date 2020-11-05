48c0c6434621060755aebda88d760619c8ae78ed
Rodrigo <rhodfra@gmail.com>
2020-11-04 23:01:44 -0600

    Finish initial data loading for both nodes n1, n2

--- bdd_prac_05-codigo/s-06-rfp-carga.sql
+++ bdd_prac_05-codigo/s-06-rfp-carga.sql
@@ -21,41 +21,54 @@ delete from F_RFP_PAGO_SUSCRIPTOR_1;
 Prompt Cargando datos
 --insert into ..
 -- Insercion de paises
-insert into F_RFP_PAIS_1 (pais_id,clave,nombre,region_economica) values();
+insert into F_RFP_PAIS_1 (pais_id,clave,nombre,region_economica) 
+values(1,'MX','MEXICO','A');
 
 -- Insercion de suscriptores 1
-insert into F_RFP_SUSCRIPTOR_1 (suscriptor_id,num_tarjeta)  values ();
+insert into F_RFP_SUSCRIPTOR_1 (suscriptor_id,num_tarjeta)  
+values (1,5420900754028724);
+
+insert into F_RFP_SUSCRIPTOR_1 (suscriptor_id,num_tarjeta)  
+values (2,5800807976301529);
+
+insert into F_RFP_SUSCRIPTOR_1 (suscriptor_id,num_tarjeta)  
+values (3,6202870129036021);
 
 -- Insercion de suscriptores 2
 insert into F_RFP_SUSCRIPTOR_2 (
   suscriptor_id,nombre,ap_paterno,ap_materno,fecha_inscripcion,pais_id
-) values ();
+) values (1,'OMAR','LIPEZ','MENDEZ',to_date('01/01/2017','DD/MM/YYYY'),1);
 
 -- Insercion de suscriptores 3
 insert into F_RFP_SUSCRIPTOR_3 (
   suscriptor_id,nombre,ap_paterno,ap_materno,fecha_inscripcion,pais_id
-) values ();
+) values (2,'LALO','KIM','LUNA',to_date('01/01/2016','DD/MM/YYYY'),2);
 
 -- Insercion en revista 1
 insert into F_RFP_REVISTA_1 (
-  revista_id,folio,revista_adicional_id,titulo,fecha_publicacion
-) values ();
+  revista_id,folio,titulo,fecha_publicacion,revista_adicional_id
+) values (1,'90001','Premier',to_date('01/03/2017','DD/MM/YYYY'),null);
 
 -- insercion en articulo 2
 insert into F_RFP_ARTICULO_2 (
   articulo_id, titulo, resumen, texto
-) values ();
+) values (1,'LOS SISMOS','Estudio y origen de los sismos',
+'Texto de ejemplo para el artículo');
+
+insert into F_RFP_ARTICULO_2 (
+  articulo_id, titulo, resumen, texto
+) values (2,'FAUNA MARINA','Estudio de la fauna marina de México.',
+'Texto de ejemplo para el artículo');
 
 -- Insercion en articulo_revista 1
 insert into F_RFP_ARTICULO_REVISTA_1 (
-  articulo_revista_id, fecha_aprobacion,calificacion
-  revista_id, articulo_id
-) values ();
+  articulo_revista_id, articulo_id, revista_id, fecha_aprobacion, calificacion
+) values (1,1,1,to_date('01/02/2017','DD/MM/YYYY'),9);
 
 -- Insercion en pago_suscriptor 1
 insert into F_RFP_PAGO_SUSCRIPTOR_1 (
   num_pago, suscriptor_id, fecha_pago, importe,recibo_pago
-) values ();
+) values (1,1,to_date('01/02/2017','DD/MM/YYYY'),989.67,empty_blob());
 
 
 --hacer commit al terminar
@@ -80,33 +93,37 @@ Prompt Cargando datos
 --insert into ..
 
 -- Insercion de pais 2
-insert into F_RFP_PAIS_2 (pais_id,clave,nombre,region_economica) values();
+insert into F_RFP_PAIS_2 (pais_id,clave,nombre,region_economica) 
+values(2,'JAP','JAPON','B');
 
 -- Insercion de suscriptores 4
 insert into F_RFP_SUSCRIPTOR_4 (
   suscriptor_id,nombre,ap_paterno,ap_materno,fecha_inscripcion,pais_id
-) values ();
+) values (3,'LUCY','ZAMORA','PEREZ',to_date('01/01/2015','DD/MM/YYYY'),2);
 
 -- Insercion en revista 2
 insert into F_RFP_REVISTA_2 (
-  revista_id,folio,revista_adicional_id,titulo,fecha_publicacion
-) values ();
+  revista_id,folio,titulo,fecha_publicacion,revista_adicional_id
+) values (2,'90002','TI en la UNAM',to_date('01/09/2017','DD/MM/YYYY'),1);
 
 -- insercion en articulo 1
 insert into F_RFP_ARTICULO_1 (
-  pdf
-) values ();
+  articulo_id,pdf
+) values (1,empty_blob());
+
+insert into F_RFP_ARTICULO_1 (
+  articulo_id,pdf
+) values (2,empty_blob());
 
 -- Insercion en articulo_revista 2
 insert into F_RFP_ARTICULO_REVISTA_2 (
-  articulo_revista_id, fecha_aprobacion,calificacion
-  revista_id, articulo_id
-) values ();
+  articulo_revista_id,articulo_id,revista_id, fecha_aprobacion,calificacion 
+) values (2,2,2,to_date('01/08/2017','DD/MM/YYYY'),10);
 
 -- Insercion en pago_suscriptor 2
 insert into F_RFP_PAGO_SUSCRIPTOR_2 (
   num_pago, suscriptor_id, fecha_pago, importe,recibo_pago
-) values ();
+) values (70,2,to_date('01/08/2017','DD/MM/YYYY'),1000.55,empty_blob());
 
 --hacer commit al terminar
 commit;
